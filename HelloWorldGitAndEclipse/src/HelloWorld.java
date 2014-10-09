103110f04f583901eb87c99b547185803f1b500e
RobertJuergens <robert.juergens@outlook.com>
2014-10-09 12:15:41 +0100

    text output changed

--- HelloWorldGitAndEclipse/src/HelloWorld.java
+++ HelloWorldGitAndEclipse/src/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
 		System.out.println("This is the first change.");
-
+		System.out.println("This is the second change.s");
 	}
 }
